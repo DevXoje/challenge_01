7d7cb60b1fad48cbe1a6eff6d20ceaa4420fc1a7
Xoje-Laptop <dawxoje@gmail.com>
2023-11-04 19:14:43 +0100

    completed

--- index.js
+++ index.js
@@ -7,33 +7,38 @@ const assert = require("assert");
  */
 function deCryptStrange(messageCrypt) {
   let messageDecrypt = "";
+  const regexToClean = new RegExp("[\n\t]", "g");
   const messageCryptLower = messageCrypt.toLowerCase();
-  const words = messageCryptLower.split(" ");
+  const messageCryptClean = messageCryptLower.replace(regexToClean, "");
+  const words = messageCryptClean.split(" ");
+  /* let messageDecrypt = "";
+  const messageCryptLower = messageCrypt.toLowerCase();
+  const words = messageCryptLower.split(" "); */
   // const filterWords = words.filter((word) => word !== " ");
   const wordsUnique = [...new Set(words)];
   const messageAlmostDecrypted = [];
   for (const element of wordsUnique) {
     const regex = new RegExp(`\\b${element}\\b`, "gi");
     const result = messageCryptLower.match(regex);
-
     let name = "";
     try {
       name = result[0];
+      const numAppearances = result.length;
+      const record = `${name}${numAppearances}`;
+      messageAlmostDecrypted.push(record);
     } catch (error) {
       console.log({
         result,
         regex,
         name,
+        messageCrypt,
       });
     }
-    const numAppearances = result.length;
-    const record = `${name}${numAppearances}`;
-    messageAlmostDecrypted.push(record);
   }
   messageDecrypt = messageAlmostDecrypted.join("");
   return messageDecrypt;
 }
-/* const example1 = {
+const example1 = {
   mock: "llaveS casa CASA casa llaves",
   expected: "llaves2casa3",
 };
@@ -45,17 +50,22 @@ const example3 = {
   mock: "casas casa casasas",
   expected: "casas1casa1casasas1",
 };
+const example4 = {
+  mock: "halcon halcon\n",
+  expected: "halcon2",
+};
 // Prueba 1
 assert.equal(deCryptStrange(example1.mock), example1.expected);
 // Prueba 2
 assert.equal(deCryptStrange(example2.mock), example2.expected);
 // Prueba 3
 assert.equal(deCryptStrange(example3.mock), example3.expected);
- */
-console.log("Todas las pruebas pasaron");
+// Prueba 4
+assert.equal(deCryptStrange(example4.mock), example4.expected);
+
+//console.log("Todas las pruebas pasaron");
 
 const axios = require("axios");
-const { log } = require("console");
 
 const url = "https://codember.dev/data/message_01.txt";
 
